a502f4dde805340671c28dc53adfd44fb1fc4792
praji <prajilap2000@gmail.com>
2026-04-26 22:36:58 +0530

    added feature like send mail and pdf downlod

--- design/urls.py
+++ design/urls.py
@@ -1,5 +1,7 @@
-from django.urls import re_path
+from django.urls import re_path, path
 from .import views
 urlpatterns = [
         re_path(r'^$', views.index, name='index'),
+        path('send-message/', views.send_message, name='send_message'),
+        path('download-resume/', views.download_resume, name='download_resume'),
            ]
--- design/views.py
+++ design/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+import os
+
+from django.core.mail import send_mail
+from django.http import FileResponse
+from django.shortcuts import render, redirect
+from django.conf import settings
 
 from .models import Experience, Project
 
@@ -10,3 +15,36 @@ def index(request):
     projects=Project.objects.all()
     return render(request, 'index.html',
                   {'experiences':experiences,'projects': projects})
+
+
+def send_message(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        project_type = request.POST.get("type")
+        message = request.POST.get("message")
+
+        full_message = f"""
+        Name: {name}
+        Email: {email}
+        Project Type: {project_type}
+
+        Message:
+        {message}
+        """
+
+        send_mail(
+            subject=f"New Contact Message from {name}",
+            message=full_message,
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[settings.EMAIL_HOST_USER],
+        )
+
+        return redirect("index")  # or show success message
+
+    return redirect("index")
+
+
+def download_resume(request):
+    file_path = os.path.join(settings.BASE_DIR, 'static/files/resume.pdf')
+    return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='Prajila_Resume.pdf')
